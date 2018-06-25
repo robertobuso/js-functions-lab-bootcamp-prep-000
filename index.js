f3f63a90ae86d43c70ff9404d9e98e43d13ab7d9
Roberto Busó-García <robertobuso@gmail.com>
2018-06-25 16:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // write your code below!
 function happyHolidays () {
-  return "Happy holidays!"
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo (name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
-
-happyHolidaysTo ("you")
 
 function happyHolidayto (holiday, name) {
   return "Happy ${holiday}, ${name}!"
